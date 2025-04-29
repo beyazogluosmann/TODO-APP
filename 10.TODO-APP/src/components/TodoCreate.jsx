71e3687a9286739e1accdf203279af8e2ec0faf8
beyazogluosmann <byzgluosmann@gmail.com>
2025-04-29 23:15:05 +0300

    TODO-APP
    react ile todo yapımı

---
+++ 10.TODO-APP/src/components/TodoCreate.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import '../App.css'
+
+function TodoCreate({onCreateTodo}) {
+    const [newTodo , setNewTodo] = useState('');
+
+    const clearİnput = () => {
+      setNewTodo('');
+    }
+
+    const createTodo = () => {
+         if(!newTodo) return;
+     
+        const request = {
+            id : Math.floor(Math.random() * 99999999),
+            content: newTodo
+        }
+        onCreateTodo(request)
+        clearİnput();
+    }
+  return (
+    <div className='todo-create'>
+        <input
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)} className='todo-input' type="text" placeholder='Todo giriniz'/>
+        <button onClick = {createTodo}className='todo-create-button'>Todo oluştur</button>
+    </div>
+  )
+}
+
+export default TodoCreate
